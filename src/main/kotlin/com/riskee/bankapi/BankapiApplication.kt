b6aebc32156a0933f310d9ee13c56cf6b919d302
riskiilyas <riskiilyas03@gmail.com>
2023-07-29 20:11:35 +0700

    Initial commit

---
+++ src/main/kotlin/com/riskee/bankapi/BankapiApplication.kt
@@ -0,0 +1,11 @@
+package com.riskee.bankapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/riskee/bankapi/BankapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.riskee.bankapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
