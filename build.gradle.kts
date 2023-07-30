4f84c79c6c7b0916c672217baa2961de42731c49
riskiilyas <riskiilyas03@gmail.com>
2023-07-30 09:25:38 +0700

    chore: add mysql dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("com.mysql:mysql-connector-j")
+	implementation("org.projectlombok:lombok")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.mockk:mockk:1.13.4")
 }
--- src/main/kotlin/com/riskee/bankapi/model/Bank.kt
+++ src/main/kotlin/com/riskee/bankapi/model/Bank.kt
@@ -1,14 +1,30 @@
 package com.riskee.bankapi.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.jetbrains.annotations.NotNull
 
+@Entity
+@Table
 data class Bank(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "account_number")
     @JsonProperty("account_number")
     val accountNumber: String,
 
+    @Column(name = "trust")
     @JsonProperty("trust")
     val trust: Double,
 
+    @Column(name = "default_transaction_fee")
     @JsonProperty("default_transaction_fee")
     val transactionFee: Int
-)
+) {
+    constructor() : this("", 0.0, 0)
+}
